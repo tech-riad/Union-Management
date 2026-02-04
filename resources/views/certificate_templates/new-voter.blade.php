780875e9c1c4bb18c6ea71f97732201deb2a5011
tech-riad <tech.riad45@gmail.com>
2026-02-04 21:47:57 +0600

    update

--- resources/views/certificate_templates/new-voter.blade.php
+++ resources/views/certificate_templates/new-voter.blade.php
@@ -1,187 +1,212 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<meta charset="UTF-8">
-<title>{{ $certificate_type }}-{{ $certificate_number }}</title>
+    <meta charset="UTF-8">
+    <title>{{ $certificate_type }}-{{ $certificate_number }}</title>
 
-<style>
-  body {
-    font-family: 'solaimanlipi', sans-serif;
-    background: #fdfdfd;
-    margin: 0;
-    padding: 25px;
-  }
+    <style>
+        body {
+            font-family: 'solaimanlipi', sans-serif;
+            background: #fdfdfd;
+            margin: 0;
+            padding: 25px;
+        }
 
-  .certificate {
-    border: 12px double #003366;
-    padding: 25px;
-    position: relative;
-  }
+        .certificate {
+            border: 12px double #003366;
+            padding: 25px;
+            position: relative;
+        }
 
-  .watermark {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    opacity: 0.08;
-    font-size: 80px;
-    font-weight: bold;
-    color: #003366;
-    white-space: nowrap;
-    pointer-events: none;
-  }
+        .watermark {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            opacity: 0.08;
+            font-size: 80px;
+            font-weight: bold;
+            color: #003366;
+            white-space: nowrap;
+            pointer-events: none;
+        }
 
-  table {
-    width: 100%;
-    border-collapse: collapse;
-  }
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
 
-  .header td {
-    vertical-align: middle;
-  }
+        .header td {
+            vertical-align: middle;
+        }
 
-  .qr, .photo {
-    width: 110px;
-    height: 110px;
-    border: 1px solid #444;
-    text-align: center;
-    font-size: 11px;
-  }
+        .qr,
+        .photo {
+            width: 110px;
+            height: 110px;
+            border: 1px solid #444;
+            text-align: center;
+            font-size: 11px;
+        }
 
-  .center {
-    text-align: center;
-  }
+        .center {
+            text-align: center;
+        }
 
-  .govt {
-    font-size: 20px;
-    font-weight: bold;
-  }
+        .govt {
+            font-size: 20px;
+            font-weight: bold;
+        }
 
-  .union {
-    font-size: 18px;
-    font-weight: bold;
-    margin-top: 5px;
-  }
+        .union {
+            font-size: 18px;
+            font-weight: bold;
+            margin-top: 5px;
+        }
 
-  .location {
-    font-size: 14px;
-  }
+        .location {
+            font-size: 14px;
+        }
 
-  .title {
-    margin: 12px auto;
-    display: inline-block;
-    background: #003366;
-    color: #fff;
-    padding: 6px 22px;
-    font-size: 18px;
-    font-weight: bold;
-    border-radius: 3px;
-  }
+        .title {
+            margin: 12px auto;
+            display: inline-block;
+            background: #003366;
+            color: #fff;
+            padding: 6px 22px;
+            font-size: 18px;
+            font-weight: bold;
+            border-radius: 3px;
+        }
 
-  .meta {
-    margin-top: 10px;
-    font-size: 13px;
-  }
+        .meta {
+            margin-top: 10px;
+            font-size: 13px;
+        }
 
-  .meta td {
-    padding: 4px;
-  }
+        .meta td {
+            padding: 4px;
+        }
 
-  .info {
-    margin-top: 20px;
-    font-size: 14px;
-  }
+        .info {
+            margin-top: 20px;
+            font-size: 14px;
+        }
 
-  .info td {
-    border: 1px solid #999;
-    padding: 8px;
-  }
+        .info td {
+            border: 1px solid #999;
+            padding: 8px;
+        }
 
-  .info td:first-child {
-    width: 30%;
-    font-weight: bold;
-    background: #f3f3f3;
-  }
+        .info td:first-child {
+            width: 30%;
+            font-weight: bold;
+            background: #f3f3f3;
+        }
 
-  .note {
-    margin-top: 25px;
-    text-align: justify;
-    font-size: 14px;
-    line-height: 1.8;
-  }
+        .note {
+            margin-top: 25px;
+            text-align: justify;
+            font-size: 14px;
+            line-height: 1.8;
+        }
 
-  .footer {
-    margin-top: 50px;
-  }
+        .footer {
+            margin-top: 50px;
+        }
 
-  .footer td {
-    vertical-align: top;
-    font-size: 14px;
-  }
+        .footer td {
+            vertical-align: top;
+            font-size: 14px;
+        }
 
-  .sign {
-    text-align: center;
-  }
+        .sign {
+            text-align: center;
+        }
 
-  .seal {
-    margin-top: 20px;
-    border: 1px dashed #555;
-    display: inline-block;
-    padding: 10px 20px;
-    font-size: 12px;
-  }
-</style>
+        .seal {
+            margin-top: 20px;
+            border: 1px dashed #555;
+            display: inline-block;
+            padding: 10px 20px;
+            font-size: 12px;
+        }
+
+    </style>
 </head>
 
 <body>
 
-<div class="certificate">
+    <div class="certificate">
 
-  <div class="watermark">New Voter</div>
+        {{-- <div class="watermark">New Voter</div> --}}
 
-  <table class="header">
-    <tr>
-      <td class="qr">
+        <table class="header">
+            <tr>
+                <td class="qr">
                     {!! $qr_code_image !!}
                 </td>
 
-      <td class="center">
-        <div class="govt">গণপ্রজাতন্ত্রী বাংলাদেশ সরকার</div>
-        <div class="union">{{$union_name ?? 'নাই'}}</div>
-        <div class="location">{{$union_address ?? 'নাই'}}</div>
+                <td class="center">
+                    <div class="govt">গণপ্রজাতন্ত্রী বাংলাদেশ সরকার</div>
+                    <div class="union">{{$union_name ?? 'নাই'}}</div>
+                    <div class="location">{{$union_address ?? 'নাই'}}</div>
 
-        <div class="title">{{ $certificate_type_bangla }}</div>
-      </td>
+                    <div class="title">{{ $certificate_type_bangla }}</div>
+                </td>
 
-      <td class="photo">PHOTO</td>
-    </tr>
-  </table>
+                <td class="photo">PHOTO</td>
+            </tr>
+        </table>
 
-  <table class="meta">
-    <tr>
-      <td>লাইসেন্স নম্বর: <b>{{ $certificate_number }}</b></td>
-      <td style="text-align:right">প্রদানের তারিখ: <b>{{ $issue_date }}</b></td>
-    </tr>
-  </table>
+        <table class="meta">
+            <tr>
+                <td>লাইসেন্স নম্বর: <b>{{ $certificate_number }}</b></td>
+                <td style="text-align:right">প্রদানের তারিখ: <b>{{ $issue_date }}</b></td>
+            </tr>
+        </table>
 
-  <table class="info">
-    <tr><td>Name</td><td>ABC Traders</td></tr>
-    <tr><td>Age</td><td>Date</td></tr>
-    <tr><td>Father's Name</td><td>Father Name</td></tr>
-    <tr><td>Mother's Name</td><td>Mother Name</td></tr>
-    <tr><td>Present Address</td><td>Village, Union, Upazila, District</td></tr>
-    <tr><td>Permanent Address</td><td>Village, Union, Upazila, District</td></tr>
-    <tr><td>Mobile Number</td><td>XXXXXXXXXXX</td></tr>
+        <table class="info">
+            <tr>
+                <td>নাম</td>
+                <td>{{ $applicant->name_bangla ?? 'নাই' }}</td>
+            </tr>
+            <tr>
+                <td>বয়স</td>
+                <td>{{ $applicant->age ?? 'নাই' }}</td>
+            </tr>
+            <tr>
+                <td>পিতার নাম</td>
+                <td>{{ $applicant->father_name_bangla ?? 'নাই' }}</td>
+            </tr>
+            <tr>
+                <td>মাতার নাম</td>
+                <td>{{ $applicant->mother_name_bangla ?? 'নাই' }}</td>
+            </tr>
+            <tr>
+                <td>বর্তমান ঠিকানা</td>
+                <td>{{ $applicant->present_address_bangla ?? 'নাই' }}</td>
+            </tr>
+            <tr>
+                <td>স্থায়ী ঠিকানা</td>
+                <td>{{ $applicant->permanent_address_bangla ?? 'নাই' }}</td>
+            </tr>
+            <tr>
+                <td>মোবাইল নম্বর</td>
+                <td>{{ $applicant->mobile_number ?? 'নাই' }}</td>
+            </tr>
+        </table>
 
-  </table>
 
-  <div class="note">
-    This is to certify that the above-mentioned individual is hereby granted a Trade License
-    to conduct the stated business within the jurisdiction of Telihati Union Parishad.
-    This license is issued according to the existing rules and regulations of the Local Government.
-  </div>
+        <div class="note">
+            এই মর্মে প্রত্যয়ন করা যাচ্ছে যে, উপরোক্ত ব্যক্তিকে তেলিহাটি ইউনিয়ন পরিষদের
+            অধিক্ষেত্রের আওতাভুক্ত এলাকায় উল্লিখিত ব্যবসা পরিচালনার জন্য ট্রেড লাইসেন্স প্রদান করা হলো।
+            এই লাইসেন্স স্থানীয় সরকার কর্তৃক প্রণীত বিদ্যমান বিধি-বিধান অনুযায়ী ইস্যু করা হয়েছে।
 
-  <table class="footer">
+        </div>
+
+        <table class="footer">
             <tr>
                 <td>
                     ইস্যুকারী:<br><br>
@@ -201,7 +226,8 @@
             </tr>
         </table>
 
-</div>
+    </div>
 
 </body>
+
 </html>
